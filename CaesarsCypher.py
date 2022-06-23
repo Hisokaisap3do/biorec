9d979b66ee0a067b9a09214a0cb1a7a4034c3c01
Hisokaisap3do <91955825+Hisokaisap3do@users.noreply.github.com>
2022-06-23 18:12:27 +0530

    Update CaesarsCypher.py

--- CaesarsCypher.py
+++ CaesarsCypher.py
@@ -1,6 +1,6 @@
-a = list(input())
+a = input()
 codeordec = int(input())
-key = int(input())
+key = 14
 if codeordec == 1:
     key  = -key
 for i in a:
@@ -8,7 +8,7 @@ for i in a:
         if i.islower():
             orn = 97 + (ord(i)-97+key)%26
             print(chr(orn),end = "")
-        else:
+        if i.isupper():
             orn = 65 + (ord(i)-65+key)%26
             print(chr(orn),end = "")
     else:
